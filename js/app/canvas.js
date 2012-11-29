7aeb82ecd8e3dbfa5aa38e0a72beb405159bafda
antoniopuero <antonio.puero@gmail.com>
2012-11-29 16:49:30 +0200

    dnd in canvas for workflow

--- js/app/canvas.js
+++ js/app/canvas.js
@@ -1,41 +1,67 @@
-$(document).ready(function () {
+var newCanvas = function () {
 	"use strict";
-	var hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'],
-		canvas = $('#plot1'),
-		ctx = canvas.get(0).getContext('2d'),
-		x,
-		maxX,
-		y,
-		maxY,
-		increment = 5,
-		randomColor = function () {
-			var randColor = '#',
-				i;
-			for (i = 0; i < 6; i += 1) {
-				randColor += hex[Math.ceil(Math.random() * 15)];
+	var canvas,
+		ctx,
+		offset,
+		isMouseDown = false,
+		cw = 0,
+		ch = 0,
+		pw = 30,
+		ph = 30,
+		px = 50,
+		py = 100,
+		cx = 0,
+		cy = 0,
+		render = function () {
+			ctx.clearRect(0, 0, cw, ch);
+			drawRect(0, 0, cw, ch, "rgb(220,220,190)");
+			drawRect(px, py, pw, ph, "blue");
+		},
+		drawRect = function (x, y, w, h, colour) {
+			ctx.fillStyle = colour;
+			ctx.beginPath();
+			ctx.rect(x, y, w, h);
+			ctx.closePath();
+			ctx.fill();
+		},
+		mouseMoved = function (e) {
+			if (isCursorOverPuck(e.pageX, e.pageY)) {
+				document.body.style.cursor = 'pointer';
+			} else {
+				document.body.style.cursor = 'default';
 			}
-			return randColor;
+			if (isMouseDown) {
+				px = (e.pageX - offset.left) - cx;
+				py = (e.pageY - offset.top) - cy;
+			}
+		},
+		mouseUp = function () {
+			isMouseDown = false;
+		},
+		mouseDown = function (e) {
+			if (isCursorOverPuck(e.pageX, e.pageY)) {
+				cx = (e.pageX - offset.left) - px;
+				cy = (e.pageY - offset.top) - py;
+				isMouseDown = true;
+			}
+		},
+		isCursorOverPuck = function (x, y) {
+			return (x - offset.left) > px && (x - offset.left) < px + pw
+				&& (y - offset.top) > py && (y - offset.top) < py + ph;
+		},
+		init = function (config) {
+			canvas = config.canvas;
+			ctx = canvas.get(0).getContext("2d");
+			cw = parseInt(canvas.css('width'), 10);
+			ch = parseInt(canvas.css('height'), 10);
+			offset = canvas.offset();
+			canvas.on('mouseup', mouseUp);
+			canvas.on('mousedown', mouseDown);
+			canvas.on('mousemove', mouseMoved);
+
+			return setInterval(render, 10); // repaint the canvas at intervals
 		};
-	for (x = 0, maxX = parseInt(canvas.css('width'), 10); x < maxX; x += increment) {
-		for (y = 0, maxY = parseInt(canvas.css('height'), 10); y < maxY; y += increment) {
-			ctx.fillStyle = randomColor();
-			ctx.fillRect(x, y, increment, increment);
-		}
-	}
-	canvas.on('mousemove', function (e) {
-		var color,
-			x = $('#coordinate-x'),
-			y = $('#coordinate-y'),
-			colorContainer = $('.coordinate-color'),
-			mouseX,
-			mouseY,
-			offset;
-		offset = $(this).offset();
-		mouseX = e.offsetX || (e.pageX - offset.left);
-		mouseY = e.offsetY || (e.pageY - offset.top);
-		x.text(mouseX);
-		y.text(mouseY);
-		color = ctx.getImageData(mouseX, mouseY, 1, 1).data;
-		colorContainer.css('background', 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')');
-	});
-});
+	return {
+		init: init
+	};
+};
---
+++ js/app/tempCanvasActions.js
@@ -0,0 +1,46 @@
+$(document).ready(function () {
+	"use strict";
+	var hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 'a', 'b', 'c', 'd', 'e', 'f'],
+		canvas = $('#plot1'),
+		canvas2,
+		ctx = canvas.get(0).getContext('2d'),
+		x,
+		maxX,
+		y,
+		maxY,
+		increment = 3,
+		randomColor = function () {
+			var randColor = '#',
+				i;
+			for (i = 0; i < 6; i += 1) {
+				randColor += hex[Math.ceil(Math.random() * 15)];
+			}
+			return randColor;
+		};
+	for (x = 0, maxX = parseInt(canvas.css('width'), 10); x < maxX; x += increment) {
+		for (y = 0, maxY = parseInt(canvas.css('height'), 10); y < maxY; y += increment) {
+			ctx.fillStyle = randomColor();
+			ctx.fillRect(x, y, increment, increment);
+		}
+	}
+	canvas.on('mousemove', function (e) {
+		var color,
+			x = $('#coordinate-x'),
+			y = $('#coordinate-y'),
+			colorContainer = $('.coordinate-color'),
+			mouseX,
+			mouseY,
+			offset;
+		offset = $(this).offset();
+		mouseX = e.offsetX || (e.pageX - offset.left);
+		mouseY = e.offsetY || (e.pageY - offset.top);
+		x.text(mouseX);
+		y.text(mouseY);
+		color = ctx.getImageData(mouseX, mouseY, 1, 1).data;
+		colorContainer.css('background', 'rgb(' + color[0] + ',' + color[1] + ',' + color[2] + ')');
+	});
+	canvas2 = newCanvas();
+	canvas2.init({
+		canvas: $('#plot2')
+	});
+});
